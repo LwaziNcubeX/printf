53b71c1ad54ccca761aa0d99fa5444e816ccd335
LwaziNcubeX <lwazincubex@gmail.com>
2023-06-24 17:41:44 +0200

    Added a _putchar

--- _printf.c
+++ _printf.c
@@ -12,60 +12,56 @@ int _printf(const char *format, ...)
 {
 	double d;
 	int i;
-	int count;
         int c;
-        const char *s;
+        char *s;
 	va_list args;
 	va_start(args, format);
 
 	while (*format != '\0')
 
 	{
-		count = 0;
 
 		if (*format == 'c')
 		{
 			c = va_arg(args, int);
 			_putchar(c);
-			count++;
+			
 		}
 
 		else if (*format == 's')
 		{
-			s = va_arg(args, const char*);
+			s = va_arg(args, char*);
 			_putchar('s');
 			s++;
-			count++;
+			
 		}
 		else if (*format == i)
 		{
 			i = va_arg(args, int);
 			_putchar(i);
-			i++;
-			count++;
+			
 		}
 		else if (*format == d)
 		{
 			d = va_arg(args, double);
 			_putchar(d);
-			d++;
-			count++;
+			
 		}
 
 		else if (*format == '%')
 		{
 			_putchar('%');
-			count++;
+			
 		}
 		else
 		{
 			_putchar(*format);
-			count++;
+		
 		}
 		format++;
 
 	}
 	va_end(args);
 
-	return (count);
+	return (0);
 }
